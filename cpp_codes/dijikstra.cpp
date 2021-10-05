5190b88f0911a34addea7460cbed804931731fbc
joeljsv <joelsunny495@gmail.com>
2021-10-05 11:55:51 +0530

    Updated

--- cpp_codes/dijikstra.cpp
+++ cpp_codes/dijikstra.cpp
@@ -1,87 +1,92 @@
-#include <limits.h>
-#include <stdio.h>
 
-// Number of vertices in the graph
-#define V 9
+#include<iostream>
+#include <list>
 
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+using namespace std;
+
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
 {
-	// Initialize min value
-	int min = INT_MAX, min_index;
+	int V; // No. of vertices
 
-	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
+	// Pointer to an array containing adjacency
+	// lists
+	list<int> *adj;
+public:
+	Graph(int V); // Constructor
 
-	return min_index;
+	// function to add an edge to graph
+	void addEdge(int v, int w);
+
+	// prints BFS traversal from a given source s
+	void BFS(int s);
+};
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
 }
 
-// A utility function to print the constructed distance array
-int printSolution(int dist[], int n)
+void Graph::addEdge(int v, int w)
 {
-	printf("Vertex Distance from Source\n");
-	for (int i = 0; i < V; i++)
-		printf("%d \t\t %d\n", i, dist[i]);
+	adj[v].push_back(w); // Add w to v’s list.
 }
 
-// Function that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
+void Graph::BFS(int s)
 {
-	int dist[V]; // The output array. dist[i] will hold the shortest
-	// distance from src to i
+	// Mark all the vertices as not visited
+	bool *visited = new bool[V];
+	for(int i = 0; i < V; i++)
+		visited[i] = false;
 
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-	// path tree or shortest distance from src to i is finalized
+	// Create a queue for BFS
+	list<int> queue;
 
-	// Initialize all distances as INFINITE and stpSet[] as false
-	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
+	// Mark the current node as visited and enqueue it
+	visited[s] = true;
+	queue.push_back(s);
 
-	// Distance of source vertex from itself is always 0
-	dist[src] = 0;
+	// 'i' will be used to get all adjacent
+	// vertices of a vertex
+	list<int>::iterator i;
 
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of vertices not
-		// yet processed. u is always equal to src in the first iteration.
-		int u = minDistance(dist, sptSet);
+	while(!queue.empty())
+	{
+		// Dequeue a vertex from queue and print it
+		s = queue.front();
+		cout << s << " ";
+		queue.pop_front();
 
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
-
-		// Update dist value of the adjacent vertices of the picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet, there is an edge from
-			// u to v, and total weight of path from src to v through u is
-			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
+		// Get all adjacent vertices of the dequeued
+		// vertex s. If a adjacent has not been visited,
+		// then mark it visited and enqueue it
+		for (i = adj[s].begin(); i != adj[s].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
 	}
-
-	// print the constructed distance array
-	printSolution(dist, V);
 }
 
-// driver program to test above function
+// Driver program to test methods of graph class
 int main()
 {
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+	Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
 
-	dijkstra(graph, 0);
+	cout << "Following is Breadth First Traversal "
+		<< "(starting from vertex 2) \n";
+	g.BFS(2);
 
 	return 0;
 }
