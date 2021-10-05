ad74c7610caa0297911b0a75bb28a60f7202a3f1
Yash Mathur <yash.mathur33@gmail.com>
2021-10-05 11:34:56 +0530

    Create reversestring.cpp

---
+++ cpp_codes/reversestring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstring>
+using namespace std; 
+int main() 
+{ 
+    char str[] ="Journal Dev reverse example"; 
+    strrev(str);
+    cout<<"\n"<<str; 
+    return 0;
+}
