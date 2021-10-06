5c70dbbfc1ecc77e28bf7849a1e6f4afe49ef9c2
Ignaviaa <prerna.staugustine8@gmail.com>
2021-10-06 21:37:50 +0530

    palindrome of a string

---
+++ Python_codes/palindrome_string/palindrome.py
@@ -0,0 +1,14 @@
+string=input("Enter a string:")
+length=len(string)
+mid=length//2
+rev=-1
+for a in range(mid):
+    if string[a]==string[rev]:
+        a+=1
+        rev=-1
+    else:
+        print(string,"is a palindrome")
+        break
+else:
+    print(string,"is not a palindrome")
+
