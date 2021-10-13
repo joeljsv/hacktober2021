267772f98e75a62cb762f09df0459c9cf82392cc
thifardi <38723240+thisfardi@users.noreply.github.com>
2021-10-13 11:21:57 +0700

    maincraft CLone 61 Line Only
    Just Maincraft CLone in VS

---
+++ Python_codes/Maincraft_Clone.py
@@ -0,0 +1,61 @@
+from ursina import *
+from ursina.prefabs.first_person_controller \
+  import FirstPersonController
+
+app = Ursina()
+Sky()
+player = FirstPersonController()
+window.fullscreen = True
+arm = Entity(
+  parent=camera.ui,
+  model='cube',
+  color=color.blue,
+  position=(0.75, -0.6),
+  rotation= (150, -10,6),
+  scale = (0.2,0.2,1.5)
+)
+def update():
+  if held_keys['left mouse']:
+    arm.position = (0.6, -0.5)
+  elif held_keys['right mouse']:
+    arm.position = (0.6, -0.5)
+  else:
+    arm.position = (0.75, -0.6)
+
+boxes = []
+
+for n in range(15):
+  for k in range(15):
+    box = Button(
+      position=(k, 0, n),
+      color=color.orange,
+      highlight_color=color.lime,
+      model='cube',
+      texture=
+      load_texture('assets\wood'),
+      origin_y=0.5,
+      parent=scene
+    )
+    boxes.append(box)
+
+def input(key):
+  for box in boxes:
+    if box.hovered:
+      if key == 'left mouse down':
+        newBox = Button(
+          position=
+          box.position + mouse.normal,
+          color=color.orange,
+          highlight_color=color.lime,
+          model='cube',
+          texture=
+          load_texture('assets\wood'),
+          origin_y=0.5,
+          parent=scene
+        )
+        boxes.append(newBox)
+      if key == 'right mouse down':
+        boxes.remove(box)
+        destroy(box)
+
+app.run()
