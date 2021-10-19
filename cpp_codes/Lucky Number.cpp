5451075bbd8f4e4f7174c2d2031a4d09342168f2
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2021-10-19 17:07:27 +0530

    Create Lucky Number.cpp

---
+++ cpp_codes/Lucky Number.cpp
@@ -0,0 +1,31 @@
+//question link: https://www.codechef.com/SNCKQL21/problems/LUCKYNUM/
+//Please consider this under hacktober fest 
+
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>>t;
+    
+   while(t--){
+       
+	int a,b,c;
+	cin>>a>>b>>c;
+	
+	if(a==7 || b==7 || c==7){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+        
+  }
+	return 0;
+}
